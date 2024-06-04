a1421848da39f841419f7cafabdec105f10d4f32
tscherm18 <33992986+tscherm18@users.noreply.github.com>
2024-06-04 11:16:23 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import math
+from math import pi
+
+def houseSQFT(length, width):
+    area = length*width
+    return area
+
+def circleCir(diameter):
+    circumference = diameter*(math.pi)
+    return circumference
